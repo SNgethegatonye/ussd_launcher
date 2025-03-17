89a3ac54dea8224ed20785e638bf8c4281a5b616
SNgethegatonye <gatonyestephen1@gmail.com>
2025-03-17 16:04:30 +0300

    Update ussd_launcher.dart

--- lib/ussd_launcher.dart
+++ lib/ussd_launcher.dart
@@ -2,10 +2,10 @@ import 'package:flutter/services.dart';
 import 'dart:developer' as developer;
 
 class UssdLauncher {
-  // Canal de méthode pour communiquer avec le code natif
+
   static const MethodChannel _channel = MethodChannel('ussd_launcher');
 
-// Lance une requête USSD en session unique 
+
   static Future<String?> sendUssdRequest({
     required String ussdCode,
     required int subscriptionId,
@@ -32,22 +32,7 @@ class UssdLauncher {
     }
   }
 
-  // Lance une requête USSD en session unique 
-  // static Future<String> launchUssd(String ussdCode, {int subscriptionId = -1}) async {
-  // static Future<String> launchUssd(String ussdCode, int? subscriptionId) async {
-  //   try {
-  //     final String response = await _channel.invokeMethod('sendUssdRequest', {
-  //       'ussdCode': ussdCode,
-  //       'subscriptionId': subscriptionId,
-  //     });
-
-  //     return response;
-  //   } on PlatformException catch (e) {
-  //     throw Exception('Failed to send USSD request: ${e.message}');
-  //   }
-  // }
-
-  // Lance une session USSD multi-étapes avec des options de menu
+  
   static Future<String?> multisessionUssd(
       {String? code, int? slotIndex, List<String>? options}) async {
     try {
@@ -69,31 +54,7 @@ class UssdLauncher {
     }
   }
 
-  // Envoie un message/une commande dans une session USSD multi-étapes
-  // static Future<String?> sendMessage(String message) async {
-  //   try {
-  //     final String? result =
-  //         await _channel.invokeMethod('sendMessage', {'message': message});
-  //     developer.log("............................. message : $message");
-  //     developer.log("............................. result : $result");
-  //     return result;
-  //   } on PlatformException catch (e) {
-  //     print("Failed to send USSD message: '${e.message}'.");
-  //     rethrow;
-  //   }
-  // }
-
-  // Annule la session USSD en cours
-  // static Future<void> cancelSession() async {
-  //   try {
-  //     await _channel.invokeMethod('cancelSession');
-  //   } on PlatformException catch (e) {
-  //     print("Failed to cancel USSD session: '${e.message}'.");
-  //     rethrow;
-  //   }
-  // }
-
-  /// Définit un listener pour les messages USSD reçus depuis le backend.
+ 
   static void setUssdMessageListener(Function(String) listener) {
     _channel.setMethodCallHandler((call) async {
       if (call.method == 'onUssdMessageReceived') {
@@ -103,35 +64,5 @@ class UssdLauncher {
     });
   }
 
-  // Vérifie si l'autorisation d'accessibilité est activée
-  // static Future<bool> isAccessibilityPermissionEnabled() async {
-  //   try {
-  //     final bool isEnabled =
-  //         await _channel.invokeMethod('isAccessibilityPermissionEnabled');
-  //     return isEnabled;
-  //   } on PlatformException catch (e) {
-  //     print("Failed to check accessibility permission: '${e.message}'.");
-  //     return false;
-  //   }
-  // }
-
-  // Ouvre les paramètres d'accessibilité
-  // static Future<void> openAccessibilitySettings() async {
-  //   try {
-  //     await _channel.invokeMethod('openAccessibilitySettings');
-  //   } on PlatformException catch (e) {
-  //     print("Failed to open accessibility settings: '${e.message}'.");
-  //     rethrow;
-  //   }
-  // }
-
-  static Future<List<Map<String, dynamic>>> getSimCards() async {
-    try {
-      final List<dynamic> result = await _channel.invokeMethod('getSimCards');
-      return result.map((item) => Map<String, dynamic>.from(item)).toList();
-    } on PlatformException catch (e) {
-      print("Failed to get SIM cards info: '${e.message}'.");
-      return [];
-    }
-  }
+ 
 }
